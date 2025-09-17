f637df083bf96922e0d1fd7712fec9a1a7d1628c
StenixDev <stenix.dev@gmail.com>
2025-09-17 16:21:52 +0800

    Building the static App: Forms

--- src/App.js
+++ src/App.js
@@ -24,7 +24,12 @@ function App(){
   return <div className="app">
     <div className="sidebar">
       <FriendsList />
+      
+      <FormAddFriend />
+      <Button>Add Friend</Button>
     </div>
+
+    <FormSplitBill />
     
   </div>
 }
@@ -53,8 +58,52 @@ function Friend({friend}){
     {friend.balance > 0 && <p className="green">{friend.name} owes you ${Math.abs(friend.balance)}</p>}
     {friend.balance === 0 && <p>You and your {friend.name} are even</p>}
 
-    <button className="button">Select</button>
+    <Button className="button">Select</Button>
   </li>
+}
+
+function Button({children}){
+  return <button className="button">{children}</button>
+}
+
+function FormAddFriend(){
+
+
+  return  <form className="form-add-friend">
+    <label>Friend name</label>
+    <input type="text" />
+
+    <label>🌐 Image URL</label>
+    <input type="text" />
+
+    <Button className="button">Add</Button>
+  </form>
+  
+}
+
+
+function FormSplitBill(){
+  return <form className="form-split-bill">
+    <h2>Split a bill with X</h2>
+
+    <label>💲 Bill Value</label>
+    <input type="text" />
+
+    <label>💲 Your Expense</label>
+    <input type="text" />
+
+    <label>💲 Bill Value</label>
+    <input type="text" disabled />
+
+    <label>💰 Who is paying the bill</label>
+    <select>
+      <option value='user'>You</option>
+      <option value='friend'>X</option>
+    </select>
+
+    <Button className="button">Split Bill</Button>
+
+  </form>
 }
 
 export default App
